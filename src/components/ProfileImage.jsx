42385385195c88f36e6bcf4971f03f50f141b4b0
Krists Krievins <krists.krievins@gmail.com>
2025-03-11 15:03:16 +0100

    done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,6 @@
+const ProfileImage = (props) => {
+  console.log(props);
+  return <img src={props.image} className="profile" alt="profile" />;
+};
+
+export default ProfileImage;
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = (props) => {
+  console.log(props);
+  return (
+    <span className="user">
+      <span className="name">{props.name}</span>
+      <span className="handle">{props.handle}</span>
+    </span>
+  );
+};
+
+export default User;
